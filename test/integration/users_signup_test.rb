9769f4037a503debd2d02a00f2df0ca45dabf1c9
Kollo David <kdavid77@gmail.com>
2015-06-09 09:55:16 +0300

    signup javitas a gyakorlatok alapjan

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,6 +7,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path , user: {name: "",email: "user@invalid", password: "ab",password_confirmation: "cd" }
     end
     assert_template 'users/new'
+    assert_select 'div#error-explanation'
+    assert_select 'div.field_with_errors'
   end
   
   test "valid signup information" do
@@ -19,5 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
